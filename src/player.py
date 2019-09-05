5cb1e7db3c38816d0f1126ade597ca41135fde59
joepound <14357797+joepound@users.noreply.github.com>
2019-09-05 13:09:12 -0600

    Added Player method for adding a new Item

--- src/player.py
+++ src/player.py
@@ -2,3 +2,7 @@ class Player:
     def __init__(self, name, current_room=None):
         self.name = name
         self.current_room = current_room
+        self.items = []
+
+    def add_item(self, item):
+        self.items.append(item)
