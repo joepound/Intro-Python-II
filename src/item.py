822cdf3fbce65f2bacd57f004087b155041e0454
joepound <14357797+joepound@users.noreply.github.com>
2019-09-05 13:17:22 -0600

    Added base code for on_drop method in Item

--- src/item.py
+++ src/item.py
@@ -4,4 +4,7 @@ class Item:
         self.description = description
 
     def on_take(self):
-        print(f"You have picked up {self.name}.")
+        print(f'You picked up "{self.name}".')
+
+    def on_drop(self):
+        print(f'You dropped "{self.name}".')
