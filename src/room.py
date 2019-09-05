778238f3a0b136777163b8ec46951b6320a9e12f
joepound <14357797+joepound@users.noreply.github.com>
2019-09-05 12:47:11 -0600

    Created Room constructor

--- src/room.py
+++ src/room.py
@@ -1,2 +1,16 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+
+class Room:
+    # Same order for adjacent rooms as HTML -> top-right-bottom-left (TRBL)
+    def __init__(
+        self, name, description,
+        n_to=None, e_to=None, s_to=None, w_to=None
+    ):
+        self.name = name
+        self.description = description
+        self.n_to = n_to
+        self.e_to = e_to
+        self.s_to = s_to
+        self.w_to = w_to
